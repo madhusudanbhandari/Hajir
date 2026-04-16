330626b1c2044b02932433b3bd9e2423cd85b96b
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-16 15:06:00 +0545

    middleware created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,12 @@
 const express =require('express');
 const cors=require('cors');
+const authRoutes=require('./Routes/authRoutes');
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('api/auth',authRoutes)
 app.get('/',(req,res)=>{
     res.send("API running....");
 });
