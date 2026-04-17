68a00112be491c6729dc1055add7d85c050f3dcc
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-17 11:24:18 +0545

    attendence history dashboard

--- Backend/src/Routes/attendenceRoutes.js
+++ Backend/src/Routes/attendenceRoutes.js
@@ -4,8 +4,12 @@ const router=express.Router();
 const auth=require('../middleware/authMiddleware');
 const {allowRoles}=require('../middleware/roleMiddleware');
 
-const {markAttendence}=require('../controllers/attendenceController');
+const {markAttendence,getStudentAttendence,getDashboard}=require('../controllers/attendenceController');
 
 router.post('/mark',auth, allowRoles('teacher'),markAttendence);
+
+router.get('/student/:id',auth,getStudentAttendence);
+
+router.get('dashboard',auth,allowRoles('parenet'),getDashboard)
 
 module.exports=router;
--- Backend/src/controllers/attendenceController.js
+++ Backend/src/controllers/attendenceController.js
@@ -1,5 +1,5 @@
 const {markAttendenceService}=require('../services/attendenceService');
-
+const {getAttendenceByStudent,getParentDashboard}=require('../services/attendenceService')
 
 exports.markAttendence=async(req,res)=>{
     try{
@@ -7,5 +7,23 @@ exports.markAttendence=async(req,res)=>{
         res.json(result);
     }catch(err){
         res.status(400).json({error:err.message});
+    }
+};
+
+exports.getStudentAttendence=async(req,res)=>{
+    try{
+        const data=await getAttendenceByStudent(req.params.id);
+        res.json(data);
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
+
+exports.getDashboard=async(req,res)=>{
+    try{
+        const data=await getParentDashboard(req.user.id);
+        res.json(data);
+    }catch(err){
+        res.status(500).json({error:err.message});
     }
 };
