e2768916cc68b04a33a32bc1956796e49c99ac6c
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-19 18:13:59 +0545

    api provider created

--- Frontend/flutter_application_1/lib/main.dart
+++ Frontend/flutter_application_1/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
