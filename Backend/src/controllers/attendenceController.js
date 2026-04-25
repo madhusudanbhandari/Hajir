942b6e033d7af06abd779d565f6aa55d2518a3cd
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-25 10:59:08 +0545

    attendence controller

--- Backend/src/controllers/attendenceController.js
+++ Backend/src/controllers/attendenceController.js
@@ -1,29 +1,60 @@
-const {markAttendenceService}=require('../services/attendenceService');
-const {getAttendenceByStudent,getParentDashboard}=require('../services/attendenceService')
+const {markAttendenceService,getAttendenceByClassService,getAttendenceByStudentService,getMonthlyAttendenceService}=require('../services/attendenceService');
 
-exports.markAttendence=async(req,res)=>{
+exports.markAttendenceService=async(req,res)=>{
     try{
-        const result=await markAttendenceService(req.body);
-        res.json(result);
-    }catch(err){
-        res.status(400).json({error:err.message});
+        const {classId, date, attendence}=req.body;
+        const teacherId=req.user.id;
+
+        if (!classId || !date || !attendance) {
+        return res.status(400).json({ msg: "Missing required fields" });
     }
-};
-
-exports.getStudentAttendence=async(req,res)=>{
-    try{
-        const data=await getAttendenceByStudent(req.params.id);
-        res.json(data);
-    }catch(err){
-        res.status(500).json({error:err.message});
-    }
-};
-
-exports.getDashboard=async(req,res)=>{
-    try{
-        const data=await getParentDashboard(req.user.id);
-        res.json(data);
+    const result=await markAttendenceService({
+        classId,teacherId,date,attendence
+    });
+    res.json({msg:'Attendence marked successfully',
+        data:result
+    });
     }catch(err){
         res.status(500).json({error:err.message});
     }
+    };
+    exports.getAttendenceByClass=async(req,res)=>{
+        try{
+            const {classId,date}=req.query;
+
+            const data=await getAttendenceByClassService(classId,date);
+
+            res.json(data);
+        }catch(err){
+            res.status(500).json({error:err.message});
+        }
+    };
+
+
+
+exports.getAttendanceByStudent = async (req, res) => {
+  try {
+    const { studentId } = req.params;
+
+    const data = await getAttendanceByStudentService(studentId);
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+exports.getMonthlyAttendance = async (req, res) => {
+  try {
+    const { studentId, month } = req.query;
+
+    const data = await getMonthlyAttendanceService(studentId, month);
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
